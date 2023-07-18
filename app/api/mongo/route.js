4445230c19e9bd7cbd2c44cf91ef1b2518669fa8
Svmpankaj <svmpankaj@gmail.com>
2023-07-18 23:54:14 +0530

    mongodb is connected

---
+++ app/api/mongo/route.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+import { MongoClient } from "mongodb";
+
+export async function GET(request) {
+
+
+    // Replace the uri string with your connection string.
+    const uri = "mongodb+srv://mongodb:Chhoti123@nodetuts.qc1eub9.mongodb.net/";
+
+    const client = new MongoClient(uri);
+
+    try {
+        const database = client.db('pankaj');
+        const movies = database.collection('inventory');
+
+        // Query for a movie that has the title 'Back to the Future'
+        const query = {};
+        const movie = await movies.findOne(query);
+
+        console.log(movie);
+        return NextResponse.json({ "a": 34, movie })
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
